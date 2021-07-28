20cefad671a793b476684f9b0e5028f3fbdd7c3b
laravel-expo <haithamahmad16995@gmail.com>
2021-07-29 03:31:41 +0400

    loginUpdated

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,12 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        \SocialiteProviders\Manager\SocialiteWasCalled::class => [
+            'SocialiteProviders\\Google\\GoogleExtendSocialite@handle',
+            'SocialiteProviders\\Apple\\AppleExtendSocialite@handle',
+            'SocialiteProviders\\Twitter\\TwitterExtendSocialite@handle',
+            'SocialiteProviders\\Facebook\\FacebookExtendSocialite@handle',
+        ],
     ];
 
     /**
